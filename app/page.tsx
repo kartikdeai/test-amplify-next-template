a577be9a2f9fec5a2cfc921ecce2f0c5b854bfd2
Kartik Shetty <kartik.shetty@takeda.com>
2025-05-04 17:00:00 -0400

    added storage browser

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 import { Authenticator } from '@aws-amplify/ui-react';
 Amplify.configure(outputs);
-
+import { StorageBrowser } from '../components/StorageBrowser';
 const client = generateClient<Schema>();
 
 export default function App() {
@@ -31,14 +31,19 @@ export default function App() {
     });
   }
 
-    return (
-        <Authenticator>
-            {({ signOut, user }) => (
-                <main>
-                    <h1>Hello {user?.username}</h1>
-                    <button onClick={signOut}>Sign out</button>
-                </main>
-            )}
-        </Authenticator>
-    );
+  return (
+      <Authenticator>
+        {({ signOut, user }) => (
+            <main>
+              <h1>Hello {user?.username}</h1>
+              <button onClick={signOut}>Sign out</button>
+
+              {/* StorageBrowser Component */}
+              <h2>Your Files</h2>
+              <StorageBrowser />
+
+            </main>
+        )}
+      </Authenticator>
+  );
 }
